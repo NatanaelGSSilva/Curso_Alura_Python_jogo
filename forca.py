73b0fee0b58d829060c596bc341bebf0b762b8a2
Natanael da Silva <dasilvanatanael700@gmail.com>
2020-09-24 10:53:08 -0300

    Conhecendo e trabalhando com tuplas

--- forca.py
+++ forca.py
@@ -1,6 +1,17 @@
 # https://docs.python.org/3/library/stdtypes.html#immutable-sequence-types Documentação
 # https: // docs.python.org/3/tutorial/datastructures.html Documentação
 # https://discord.gg/QYpExzq
+# dias = ["S","T","Q","Q","S","S","D"]
+# dias.append("D2") Acrecestar D2
+# dias.pop("D2")
+# dias = ()"S","T","Q","Q","S","S","D") # Lista Fixa
+#palavras = []
+# palavras.append("BANANA")
+# palavras.append("ABACAXI")
+#nova = tuple(palavras)
+# nova listar
+#outra = list(nova)
+# outra
 
 
 def jogar():
